d0d8e6a86545b224d77a5abd08fac6aa01c2671a
meenatchisekar <meenasekar336@gmail.com>
2017-06-24 10:27:05 +0530

    Add files via upload

---
+++ Vehi.java
@@ -0,0 +1,13 @@
+
+public class Vehi {
+
+	Vehi(){
+		System.out.println("Bike is created");
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Vehi v=new Vehi();
+	}
+
+}
