f78deeed0af0e6f62f15009e50f8011af251f6ca
meenatchisekar <meenasekar336@gmail.com>
2017-08-09 21:09:32 +0530

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,21 @@
+package meena;
+
+import java.util.Scanner;
+
+public class oddeven {
+
+	public static void main(String[] args) {
+		Scanner a=new Scanner(System.in);
+		int n=a.nextInt();
+		if(n%2==0)
+		{
+			System.out.println("even");
+		}
+
+	else 
+	{
+		System.out.println("odd");
+	}
+
+}
+}
